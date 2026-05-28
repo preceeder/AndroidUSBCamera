6855066537e5f1a1f0306b164f828967883179c0
wang <358435135@qq.com>
2026-05-28 09:42:48 +0800

    first commit

---
+++ libuvccommon/build/generated/source/buildConfig/release/com/jiangdg/common/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.jiangdg.common;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.jiangdg.common";
+  public static final String BUILD_TYPE = "release";
+  // Field from default config.
+  public static final String STL_NAME = "c++_shared";
+}
